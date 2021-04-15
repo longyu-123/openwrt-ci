4afedc68d8eb67232624456e0fa089738f372f54
longyu-123 <82598226+longyu-123@users.noreply.github.com>
2021-04-15 23:41:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Clash
+mkdir -p package/apps
+git clone --depth=1 https://github.com/frainzy1477/luci-app-clash.git package/apps/luci-app-clash
